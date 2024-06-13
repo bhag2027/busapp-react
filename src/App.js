0ea431416abb8caa568f07357a8184cbee1869bc
bhag2027 <bhagyazz2027@gmail.com>
2024-06-14 01:59:35 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddUser from './components.jsx/AddUser';
+import LoginUser from './components.jsx/LoginUser';
 
 function App() {
   return (
     <div >
      <BrowserRouter>
      <Routes>
+     <Route path='/' element={<LoginUser/>}/>
       <Route path='/reg' element={<AddUser/>}/>
+     
      </Routes>
      </BrowserRouter>
     </div>
---
+++ src/components.jsx/LoginUser.jsx
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const LoginUser = () => {
+
+    const [data,changeData]=useState([
+        {
+            "email":"",
+            "password":""
+        }
+     ])
+  
+  const inputHandler=(event)=>{
+      changeData({...data,[event.target.name]:event.target.value})
+  }
+  
+  const readValue=()=>{
+      console.log(data)
+      axios.post("http://localhost:8080/login",data).then(
+          (response)=>{
+              console.log(response.data)
+              if (response.data.status=="success") {
+                  alert("successfully added")
+                  
+              } else {
+                  alert("error")
+                  
+              }
+          }
+      ).catch(
+          (error)=>{
+              console.log(error)
+          }
+      )
+  }
+  return (
+    <div>
+           <h1 align="center"><u>SIGNIN</u></h1>
+            <div className="container">
+                <div class="card" >
+                   
+                        <div class="card-body">
+
+                        </div>
+                        <div className="row">
+                            <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <div className="row g-3">
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">email</label>
+                                        <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler}/>
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">Password</label>
+                                        <input type="password"  className="form-control" name='password' value={data.password} onChange={inputHandler} />
+                                    </div>
+                                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                        <button className="btn btn-success" onClick={readValue}>login</button>
+                                        <p>new users click here <Link to="/add">Sign up</Link></p>
+                                        
+                                    </div>
+                                    
+                                    
+                                </div>
+                            </div>
+                        </div>
+                </div>    </div>
+    </div>
+  )
+}
+
+export default LoginUser
